4e1cf2ef93f7b0b929960e61713733c7dd7653b7
Kelly <36lima@gmail.com>
2016-10-11 09:08:29 -0400

    Added implicit wait

--- functional_tests.py
+++ functional_tests.py
@@ -4,7 +4,7 @@ class NewVisitorTest(unittest.TestCase):
   
   def setUp(self):
     self.browser = webdriver.Chrome('Users/knetterv/Documents/chromedriver')
-
+    self.browser.implicitly_wait(3)
   def tearDown(self):
     self.browser.quit()
 
